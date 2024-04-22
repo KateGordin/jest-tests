8dea8ca30690056b3495feb03a457b1d8dfdd33a
KateGordin <kate.gordin@gmail.com>
2024-04-22 13:34:57 +0200

    Refactor variable naming in UsersService spec

--- src/app/shared/services/users.service.spec.ts
+++ src/app/shared/services/users.service.spec.ts
@@ -2,17 +2,17 @@ import { UsersService } from "./users.service";
 import { TestBed } from "@angular/core/testing";
 
 describe('UsersService', () => {
-  let UsersService: UsersService;
+  let usersService: UsersService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [UsersService],
     });
 
-    UsersService = TestBed.inject(UsersService);
+    usersService = TestBed.inject(UsersService);
   });
 
   it('creates a service', () => {
-    expect(UsersService).toBeTruthy();
+    expect(usersService).toBeTruthy();
   });
 });
