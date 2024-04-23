cec0fe61eb4adc578bdea6c7ebe524c08e4fc8ba
KateGordin <kate.gordin@gmail.com>
2024-04-23 14:50:22 +0200

    Refactor UsersService to use BehaviorSubject for users

--- src/app/shared/services/users.service.spec.ts
+++ src/app/shared/services/users.service.spec.ts
@@ -1,27 +1,19 @@
 import { UserInterface } from "../types/user.interface";
 import { UsersService } from "./users.service";
 import { TestBed } from "@angular/core/testing";
-import { UtilsService } from "./utils.service";
 
 describe('UsersService', () => {
   let usersService: UsersService;
-  let utilsService: UtilsService;
-  // const utilsServiceMock = {
-  //   pluck: jest.fn(),
-  // };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        UsersService, 
-        UtilsService
-        // { provide: UtilsService, useValue: utilsServiceMock }
+        UsersService
       ],
     });
 
     usersService = TestBed.inject(UsersService);
-    utilsService = TestBed.inject(UtilsService);
-  });
+    });
 
   it('creates a service', () => {
     expect(usersService).toBeTruthy();
@@ -34,26 +26,15 @@ describe('UsersService', () => {
         name: 'foo' 
       }
       usersService.addUser(user);
-      expect(usersService.users).toEqual([{id: '3', name: 'foo'}]);
+      expect(usersService.users$.getValue()).toEqual([{id: '3', name: 'foo'}]);
     });
   });
 
   describe('removeUser', () => {
     it('should remove a user', () => {
-      usersService.users = [{id: '3', name: 'foo'}];
+      usersService.users$.next([{id: '3', name: 'foo'}]);
       usersService.removeUser('3');
-      expect(usersService.users).toEqual([]);
-    });
-  });
-
-  describe('getUserNames', () => {
-    it('should return user names', () => {
-      jest.spyOn(usersService.utilsService, 'pluck');
-      usersService.users = [{id: '3', name: 'foo'}];
-      usersService.getUserNames();
-      expect(usersService.utilsService.pluck).toHaveBeenCalledWith(usersService.users, 'name');
-      // utilsServiceMock.pluck.mockReturnValue(['foo']);
-      // expect(usersService.getUserNames()).toEqual(['foo']);
+      expect(usersService.users$.getValue()).toEqual([]);
     });
   });
 });
--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -1,22 +1,17 @@
-import { Injectable, inject } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { UserInterface } from "../types/user.interface";
-import { UtilsService } from "./utils.service";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable()
 export class UsersService {
-  utilsService = inject(UtilsService);
-  users: UserInterface[] = [];
+  users$ = new BehaviorSubject<UserInterface[]>([]);
 
   addUser(user: UserInterface): void {
-    this.users = [...this.users, user];
+    this.users$.next([...this.users$.getValue(), user]);
   }
 
   removeUser(userId: string): void {
-    const updateUsers = this.users.filter((user) => userId !== user.id);
-    this.users = updateUsers;
-  }
-
-  getUserNames(): string[] {
-    return this.utilsService.pluck(this.users, 'name');
+    const updateUsers = this.users$.getValue().filter((user) => userId !== user.id);
+    this.users$.next(updateUsers);
   }
 }
