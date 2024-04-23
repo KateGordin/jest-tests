a9566a3daaba2bf02e74127d94c3885303b039ce
KateGordin <kate.gordin@gmail.com>
2024-04-23 17:28:45 +0200

    Add createTag test case to ApiService

--- src/app/shared/services/api.service.spec.ts
+++ src/app/shared/services/api.service.spec.ts
@@ -36,4 +36,17 @@ describe('ApiService', () => {
       expect(tags).toEqual([{ id: '1', name: 'foo' }]);
     });
   });
+
+
+  describe('createTag', () => {
+    it('should create a tag', () => {
+      let tag: TagInterface | undefined;
+      apiService.createTag('foo').subscribe(response => {
+        tag = response;
+      });
+      const req = httpTestingController.expectOne('http://localhost:3004/tags');
+      req.flush({ id: '1', name: 'foo' });
+      expect(tag).toEqual({ id: '1', name: 'foo' });
+    });
+  });
 });
