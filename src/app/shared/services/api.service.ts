f9ce9b8bc4e4439e412c0124511d69c856a75f46
KateGordin <kate.gordin@gmail.com>
2024-04-23 17:24:21 +0200

    Add ApiService unit tests

---
+++ src/app/shared/services/api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, inject } from "@angular/core";
+import { Observable } from "rxjs";
+import { TagInterface } from "../types/tag.interface";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable()
+export class ApiService {
+  httpClient = inject(HttpClient);
+  apiUrl = 'http://localhost:3004';
+
+  getTags(): Observable<TagInterface[]> {
+    return this.httpClient.get<TagInterface[]>(`${this.apiUrl}/tags`);
+  };
+
+  createTag(name: string): Observable<TagInterface> {
+    return this.httpClient.post<TagInterface>(`${this.apiUrl}/tags`, { name });
+  }
+  
+}
